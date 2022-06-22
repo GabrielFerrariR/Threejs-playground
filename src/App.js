f3cbc74cb7d9e5e7c9f348c792dc54900a56cc5c
Gabriel <gabrielferrari601@gmail.com>
2022-06-22 18:49:45 -0300

    fiz nada

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/contact" element={ <Contact/> }/>
         <Route exact path="/projects" element={ <Projects/> }/>
+        <Route exact path="/teste" />
       </Routes>
     </>
   )
